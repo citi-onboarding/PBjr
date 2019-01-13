85a143a65ede624d0e7e611c7f0a7ea93b87d2eb
Victor Aguiar <vag2@cin.ufpe.br>
2019-01-12 23:53:00 -0200

    Reduzindo distância mínima na linha do tempo

--- static/js/linha-tempo.js
+++ static/js/linha-tempo.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($){
 	var timelines = $('.cd-horizontal-timeline'),
-		eventsMinDistance = 75;
+		eventsMinDistance = 65;
 
 	(timelines.length > 0) && initTimeline(timelines);
 
